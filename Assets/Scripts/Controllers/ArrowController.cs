ad0cc5a3c4d496fc0dd8fc6f5a53f69a21e7ac9a
KirilChingarov <kiril.chingarov@gmail.com>
2025-05-17 00:01:42 +0300

    Added Environment Assets. Added Training Scene.

--- Assets/Scripts/Controllers/ArrowController.cs
+++ Assets/Scripts/Controllers/ArrowController.cs
@@ -1,47 +1,82 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
-    private PullInteractable bow;
-    private Rigidbody rigidbody;
 
     [Header("Settings")]
     public float speed = 10f;
+    public Transform tip;
 
-    private bool isAirborne = false;
+    
+    private PullInteractable _bow;
+    private Rigidbody _rigidbody;
+    private bool _isAirborne = false;
+    private Vector3 _lastPosition;
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
 
+        Stop();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isAirborne) return;
+        
+        CheckCollision();
+        _lastPosition = tip.localPosition;
+    }
+
+    private void CheckCollision()
+    {
+        if (!Physics.Linecast(_lastPosition, tip.position, out RaycastHit hit)) return;
+        print("Hit: " + hit.transform.gameObject.name);
+        if (hit.transform.gameObject.layer != 10)
+        {
+            print("Wrong layer: " + hit.transform.gameObject.layer + ", searching for: " + 10);
+            return;
+        }
+        
+        if (hit.transform.TryGetComponent(out Rigidbody hitRigidbody))
+        {
+            _rigidbody.interpolation = RigidbodyInterpolation.None;
+            transform.parent = hit.transform;
+            hitRigidbody.AddForce(_rigidbody.linearVelocity, ForceMode.Impulse);
+        }
+        else
+        {
+            print("No rigidbody");
+        }
         Stop();
     }
 
     public void RegisterBow(PullInteractable bow)
     {
-        this.bow = bow;
-        this.bow.OnRelease += Release;
+        _bow = bow;
+        _bow.OnRelease += Release;
     }
 
     private void Release(float pullAmount)
     {
-        bow.OnRelease -= Release;
+        _bow.OnRelease -= Release;
         gameObject.transform.parent = null;
-        isAirborne = true;
+        _isAirborne = true;
         SetPhysics(true);
 
-        Vector3 force = transform.forward * speed * pullAmount;
-        rigidbody.AddForce(force, ForceMode.Impulse);
+        var force = transform.forward * speed * pullAmount;
+        _rigidbody.AddForce(force, ForceMode.Impulse);
         StartCoroutine(RotateWithVelocity());
     }
 
     private IEnumerator RotateWithVelocity()
     {
         yield return new WaitForFixedUpdate();
-        while (isAirborne)
+        while (_isAirborne)
         {
-            Quaternion newRotation = Quaternion.LookRotation(rigidbody.linearVelocity, transform.up);
+            var newRotation = Quaternion.LookRotation(_rigidbody.linearVelocity, transform.up);
             transform.rotation = newRotation;
             yield return null;
         }
@@ -49,13 +84,13 @@ public class ArrowController : MonoBehaviour
 
     private void Stop()
     {
-        isAirborne = false;
+        _isAirborne = false;
         SetPhysics(false);
     }
 
     private void SetPhysics(bool usePhysics)
     {
-        rigidbody.useGravity = usePhysics;
-        rigidbody.isKinematic = !usePhysics;
+        _rigidbody.useGravity = usePhysics;
+        _rigidbody.isKinematic = !usePhysics;
     }
 }
